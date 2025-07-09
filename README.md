85ab46dce7b5209b69207007515642a881b88208
kathhill <kath.hill@sbcglobal.net>
2025-07-09 10:59:58 -0700

    feat: Initial titan-core theme commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# pathfinderarts
+# PATHFINDER ARTS COLLECTIVE
+
+## Built with Astro, TailwindCSS, Typescript
+
+## Third Party Services
+
+- [AOS Animation Library](https://michalsnik.github.io/aos/)
+- [Lucide](https://lucide.dev/)
---
+++ src/data/config.ts
@@ -0,0 +1,27 @@
+export const siteConfig = {
+  companyName: 'Pathfinder Arts Collective',
+  siteUrl: 'https://pathfinderarts.org',
+  Socials: {
+      xSocial: 'https://x.com/astrodotbuild',
+      Github: 'https://github.com/withastro',
+      Instagram: 'https://www.instagram.com/pathfinderarts/',
+      LinkedIn: 'https://www.linkedin.com/company/pathfinderarts/',
+      Email: 'support@titan-core.com',
+      Phone: '+123 456 7890',
+      Location: 'San Diego, CA, USA',
+  }
+};
+
+export const SEO = {
+  Separator: '|',
+  SiteName: 'Pathfinder Arts Collective',
+  defaultDescription: 'Nonprofit Board Consulting. Bold Governance. Shared Power.',
+};
+  
+export const blogSetting = {
+  postsPerPage: 6   
+}
+
+export const themeSetting = {
+  theme: 'pathfinder'
+}
