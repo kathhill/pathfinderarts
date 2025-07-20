09d3facc71b90d8d53f03093a98321a7388f876b
kathhill <kath.hill@sbcglobal.net>
2025-07-20 15:00:24 -0700

    chore: Configure for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,48 +7,51 @@ import sitemap from '@astrojs/sitemap';
 const site = process.env.PUBLIC_SITE_URL || 'http://localhost:4321';
 
 export default defineConfig({
-  site,
-  vite: {
-    plugins: [tailwindcss()],
-    css: {
-      preprocessorOptions: {
+    // temp github pages deployment
+    site: 'https://kathhill.github.io',
+    base: '/pathfinderarts',
+    // end temp github page deployment
+    vite: {
+        plugins: [tailwindcss()],
         css: {
-          additionalData: `@import "aos/dist/aos.css";`
-        }
-      }
+            preprocessorOptions: {
+                css: {
+                    additionalData: `@import "aos/dist/aos.css";`,
+                },
+            },
+        },
+        optimizeDeps: {
+            include: ['aos'],
+        },
     },
-    optimizeDeps: {
-      include: ['aos']
-    }
-  },
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark',
-      wrap: true,
-      langs: [],
-      transformers: [],
-      showLineNumbers: false,
-      lineNumbersPrefix: ''
-    }
-  },
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      filter: (page) => !page.includes('/404'), // Only exclude 404 page
-      entryLimit: 10000, // Increase entry limit if you have many pages
-    }),
-  ],
-  image: {
-    // Allow all remote patterns (https and http)
-    remotePatterns: [
-      {
-        protocol: "https"
-      },
-      {
-        protocol: "http"
-      }
-    ]
-  }
+    markdown: {
+        shikiConfig: {
+            theme: 'github-dark',
+            wrap: true,
+            langs: [],
+            transformers: [],
+            showLineNumbers: false,
+            lineNumbersPrefix: '',
+        },
+    },
+    integrations: [
+        sitemap({
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date(),
+            filter: (page) => !page.includes('/404'), // Only exclude 404 page
+            entryLimit: 10000, // Increase entry limit if you have many pages
+        }),
+    ],
+    image: {
+        // Allow all remote patterns (https and http)
+        remotePatterns: [
+            {
+                protocol: 'https',
+            },
+            {
+                protocol: 'http',
+            },
+        ],
+    },
 });
